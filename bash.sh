7204ed80dad7e15bd6a0889d756fe04b49e53871
th4s <th4s@metavoid.xyz>
2023-08-30 11:42:26 +0200

    Added commit signing

--- bash.sh
+++ bash.sh
@@ -23,6 +23,10 @@ CDPATH=:~
 # Do not store commands starting with space in bash history
 HISTCONTROL="ignorespace"
 
+#Some terminal setting to make signing commits work
+GPG_TTY=$(tty)
+export GPG_TTY
+
 # Set nvim as our editor
 if [[ -x "$(command -v nvim)" ]]; then
     export EDITOR=/usr/bin/nvim
